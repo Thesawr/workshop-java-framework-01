3ad700fd129bfa8b18a59937f09e2529a5718935
KBANKNET\sarun.w <sarun.w@kbtg.tech>
2021-03-01 14:49:09 +0700

    add test case

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,38 @@
-package com.example.demo;
+package main.java.com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Random;
 
-@SpringBootApplication
 public class DemoApplication {
+    private Random random;
+    public void setRandom(Random random) {
+        this.random = random;
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public String generateData(String name) {
+        int number = random.nextInt(10); // 0-9
+        if(number >= 5 && number<=8) {
+            return name + number;
+        }
+        throw new RuntimeException("Invalid number with " + number);
+    }
 
+//    public static void main(String[] args) {
+//        // Anonymous class + Override by methods
+//        Random random = new Random(){
+//            @Override
+//            public int nextInt(int bound) {
+//                return 5;
+//            }
+//        };
+//        DemoService demoService = new DemoService();
+//        demoService.setRandom(new Random5());
+//        out.println(demoService.generateData("somkiat"));
+//    }
 }
+
+//class Random5 extends Random {
+//    @Override
+//    public int nextInt(int bound) {
+//        return 5;
+//    }
+//}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,48 @@
-package com.example.demo;
+package test.java.com.example.demo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+import main.java.com.example.demo.DemoApplication;
 
-	@Test
-	void contextLoads() {
-	}
+import java.util.Random;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DemoApplicationTests {
+
+    @Test
+    @DisplayName("ในการทำงานต้อง random ได้ค่า 5")
+    public void random_5() {
+    	DemoApplication demoService = new DemoApplication();
+        demoService.setRandom(new Random5());
+        String actualResult =demoService.generateData("somkiat");
+        assertEquals("somkiat5", actualResult);
+    }
+    
+    @Test
+    @DisplayName("ในการทำงานต้อง random ได้ค่า 4")
+    public void random_4() {
+    	DemoApplication demoService = new DemoApplication();
+        demoService.setRandom(new Random4());
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            String actualResult = demoService.generateData("somkiat");
+        });
+    }
+}
+    
+
+
+class Random5 extends Random {
+    @Override
+    public int nextInt(int bound) {
+        return 5;
+    }
+}
+
+class Random4 extends Random {
+    @Override
+    public int nextInt(int bound) {
+        return 4;
+    }
 }
